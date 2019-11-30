be6da5fff3fcd9b4b05885fcd34071b9d0087efe
jmboite <jeanmarc.boite@gmail.com>
2019-11-30 14:49:11 +0100

    remove unused

--- internal/utils/display.go
+++ internal/utils/display.go
@@ -9,7 +9,6 @@ import (
 	// "github.com/rs/xid"
 )
 
-// https://codepen.io/bisserof/pen/fdtBm
 type Debug struct {
 	data map[string]interface{}
 }
@@ -110,100 +109,3 @@ func arrayToHTML(a []interface{}) string {
 `)
 	return bufferString.String()
 }
-
-const styleSheet = `
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
-<style>
-    /* https://makina-corpus.com/blog/metier/2014/construire-un-tree-view-en-css-pur  */
-    /* fonctionnel */
-    
-    input {
-        display: none;
-    }
-    
-    input~ul {
-        display: none;
-    }
-    
-    input:checked~ul {
-        display: block;
-    }
-    
-    input~ol {
-        display: none;
-    }
-    
-    input:checked~ol {
-        display: block;
-    }
-    
-    input~.tree-node-down {
-        display: none;
-    }
-    
-    input:checked~.tree-node-right {
-        display: none;
-    }
-    
-    input:checked~.tree-node-down {
-        display: inline;
-    }
-    /* habillage */
-    
-    li {
-        display: block;
-        font-family: 'Arial';
-        font-size: 15px;
-        padding: 0.2em;
-        border: 1px solid grey;
-    }
-    
-    li:hover {
-        border: 3px solid black;
-        border-radius: 3px;
-        background-color: lightgrey;
-    }
-</style>
-`
-
-const list = `
-
-<ul>
-    <li><input type="checkbox" id="c1" />
-        <i class="fa fa-angle-double-right tree-node-right"></i>
-        <i class="fa fa-angle-double-down tree-node-down"></i>
-        <label for="c1">Dossier A</label>
-        <ul>
-            <li>Sous dossier A1</li>
-            <li>Sous dossier A2</li>
-            <li>Sous dossier A3</li>
-        </ul>
-    </li>
-    <li><input type="checkbox" id="c2" />
-        <i class="fa fa-angle-double-right tree-node-right"></i>
-        <i class="fa fa-angle-double-down tree-node-down"></i>
-        <label for="c2">Dossier B</label>
-        <ul>
-            <li>Sous dossier B1</li>
-            <li><input type="checkbox" id="c3" />
-                <i class="fa fa-angle-double-right tree-node-right"></i>
-                <i class="fa fa-angle-double-down tree-node-down"></i>
-                <label for="c3">Sous dossier B2</label>
-                <ul>
-                    <li>Sous-sous dossier B21</li>
-                    <li><input type="checkbox" id="c4" />
-                        <i class="fa fa-angle-double-right tree-node-right"></i>
-                        <i class="fa fa-angle-double-down tree-node-down"></i>
-                        <label for="c4">Sous-sous dossier B22</label>
-                        <ul>
-                            <li>Sous-sous-sous dossier B221</li>
-                            <li>Sous-sous-sous dossier B222</li>
-                        </ul>
-                    </li>
-                    <li>Sous-sous dossier B23</li>
-                </ul>
-            </li>
-        </ul>
-    </li>
-</ul>
-`
